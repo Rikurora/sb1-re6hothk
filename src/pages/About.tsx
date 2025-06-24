737898b35dec4e2446cea51f7dc8c72c9ca65359
Sergio-Jim <jimmy1sergio2@gmail.com>
2025-06-24 15:47:25 +0200

    fixed broken css, banner color was not displaying

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -97,8 +97,8 @@ const About: React.FC = () => {
 
   return (
     <div>
-      {/* Page Header */}relative bg-gradient-to-r from-ncrst-blue to-ncrst-green text-white
-      <section className=" py-16 h-[300px] flex items-center justify-center">
+      {/* Page Header */}
+      <section className="relative bg-gradient-to-r from-ncrst-grey to-ncrst-gold text-white py py-16 h-[300px] flex items-center justify-center">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h1 className="text-4xl md:text-5xl font-bold mb-6 leading-heading">
